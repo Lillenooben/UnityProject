8528468060a26505c821184d2abd64dac7ff74b7
Lillenooben <jesperli99@hotmail.com>
2016-11-26 13:52:19 +0100

    Added rb.AddRelativeForce
    No longer using vector3.Forward

--- Unity spel/FlyAway-Albin2k∞/Assets/Scripts/FlyController.cs
+++ Unity spel/FlyAway-Albin2k∞/Assets/Scripts/FlyController.cs
@@ -4,16 +4,21 @@ using System.Collections;
 public class FlyController : MonoBehaviour
 {
     public float speed;
+    int timer = 0;
+    int turnTimer = 0;
+
+    public Rigidbody rb;
 
     // Use this for initialization
     void Start()
     {
-
+        rb.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        rb.AddRelativeForce(new Vector3(0, 10, 15));
 
         TransformMovement();
     }
@@ -21,20 +26,34 @@ public class FlyController : MonoBehaviour
     void TransformMovement()
     {
 
+        Debug.Log(transform.rotation.x);
+
         if (Input.GetKey(KeyCode.W))
-            gameObject.transform.Translate(Vector3.forward * speed);
-        else
         {
-            
-            gameObject.transform.Translate(Vector3.forward * 0.15f);
+            rb.AddRelativeForce(new Vector3(10, 0, 0));
+            timer = 0;
+            gameObject.transform.Rotate(new Vector3(-1, transform.rotation.y, transform.rotation.z));
         }
-            
+
+        else if (Input.GetKey(KeyCode.S))
+        {
+            //gameObject.transform.
+
+
+            //if (transform.rotation.x < 0.20f)
+            //{
+            gameObject.transform.Rotate(new Vector3(1, transform.rotation.y, transform.rotation.z));
+            //}
+
+        }
+
 
         if (Input.GetKey(KeyCode.E))
-            gameObject.transform.Rotate(new Vector3(0, 1, 0));
+            gameObject.transform.Rotate(new Vector3(0, 1, 1));
 
         if (Input.GetKey(KeyCode.Q))
-            gameObject.transform.Rotate(new Vector3(0, -1, 0));
+            gameObject.transform.Rotate(new Vector3(0, -1, -1));
+
 
 
         //if (Input.GetKey(KeyCode.S))
